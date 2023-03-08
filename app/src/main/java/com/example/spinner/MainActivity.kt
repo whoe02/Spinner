decef8f3f9e42e7f209bac01dc462b8d1c84d3ae
anreh <anurehka@gmail.com>
2023-03-08 11:52:35 +0800

    Spinner dropdown first try

---
+++ app/src/main/java/com/example/spinner/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.spinner
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val spinnerChoice : Spinner = findViewById(R.id.spinner)
+        val list_of_items = arrayOf("Coffee","tea","Utar milo")
+
+        val aa = ArrayAdapter(this, android.R.layout.simple_spinner_item, list_of_items)
+        aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinnerChoice!!.setAdapter(aa)
+    }
+}
